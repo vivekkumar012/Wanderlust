91ab1cab7c615c0b518727922f3022e60d7bb55f
Vivek Kumar <154126190+vivekkumar012@users.noreply.github.com>
2025-02-17 19:24:39 +0530

    Update index.js

--- init/index.js
+++ init/index.js
@@ -1,22 +1,36 @@
-const mongoose=require('mongoose');
-const initdata=require('./data');
-const listing=require('../models/listing');
+const mongoose = require('mongoose');
+const initdata = require('./data');
+const listing = require('../models/listing');
 
-const mongoURL='mongodb://127.0.0.1:27017/wanderlust';
+const mongoURL = 'mongodb+srv://i_vivek_07:vWFSIK8qkavOWGvB@cluster0.lomqe.mongodb.net/wanderlust?retryWrites=true&w=majority&appName=Cluster0';
 
-main().then(()=>{
-    console.log('Connect the DataBase');
-}).catch((err)=>{
-    console.log(err);
+main().then(() => {
+    console.log('Connected to the Database');
+}).catch((err) => {
+    console.error('MongoDB Connection Error:', err);
 });
 
 async function main() {
-    await mongoose.connect(mongoURL);
+    await mongoose.connect(mongoURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
 }
-const initDb= async()=>{
-    await listing.deleteMany({});
-    initdata.data=initdata.data.map((obj)=>({...obj,owner:"65a7a913ac24ac23c6839a1c"}));
-    await listing.insertMany(initdata.data);
-    console.log('data was initialised');
-}
+
+const initDb = async () => {
+    try {
+        await listing.deleteMany({});
+        initdata.data = initdata.data.map((obj) => ({
+            ...obj,
+            owner: "65a7a913ac24ac23c6839a1c"
+        }));
+        await listing.insertMany(initdata.data);
+        console.log('Data was initialized');
+    } catch (error) {
+        console.error('Error initializing data:', error);
+    } finally {
+        mongoose.connection.close(); // Close connection after seeding
+    }
+};
+
 initDb();
