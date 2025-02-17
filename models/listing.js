a018105fcd929c3b4035cbdb4d866a5d58d5e2a6
Vivek Kumar <vh118854@gmail.com>
2025-02-17 18:40:16 +0530

    first commit

---
+++ models/listing.js
@@ -0,0 +1,56 @@
+const mongoose=require('mongoose');
+// const {Schema}=mongoose;
+const Schema = mongoose.Schema;
+
+const review=require('./review');
+const { required } = require('joi');
+
+const listingSchema=new Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:String,
+    image:{
+        filename:String,
+        url:String,
+    },
+    category:{
+        type:String,
+        enum:['Trending','Rooms','Iconic Center','mountains','castles','Amazing pools','Camping','farms','Arctic','Domes','Boats','Creative Spaces','Golfing','Beach'],
+        required:true,
+    },
+    price:Number,
+    location:String,
+    country:String,
+    reviews:[{
+        type:Schema.Types.ObjectId,
+        ref:'review',
+    },],
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:'user',
+        required: true
+    },
+    // geometry:{
+    //     type:{
+    //         type:String,
+    //         enum:['Point'],
+    //         required:true
+    //     },
+    //     coordinates:{
+    //         type:[Number],
+    //         required:true,
+    //     }
+    // }
+   
+});
+
+listingSchema.post('findOneAndDelete',async(listing)=>{
+    if(listing){
+        await review.deleteMany({_id:{$in:listing.reviews}});
+    }
+});
+
+const listing = mongoose.model('listing',listingSchema);
+module.exports=listing;
